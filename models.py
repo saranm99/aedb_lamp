6b2b7c8f685f41ea422eaf51bb96ea945e797438
saranm99 <58508024+saranm99@users.noreply.github.com>
2020-04-16 12:32:42 +0100

    Update models.py

--- models.py
+++ models.py
@@ -2,5 +2,39 @@ class Lamp:
     def __init__(self):
         self.state = False
     
-    def turn_on(self):
+    def set_on(self):
         self.state = True
+    
+    def set_off(self):
+        self.state = False
+    
+    def get_state(self):
+        return self.state
+
+class colourLamp:
+    
+    def __init__(self):
+        self.colour = "White" #default colour
+        Lamp.__init__(self)
+
+    def set_colour(self, colour):
+        self.colour = colour
+
+    def get_colour(self):
+        return self.colour
+
+class LampArray:
+    def __init__(self):
+        self.conjunto = []
+    
+    def append_lamp(self, lamp):
+        self.conjunto.append(lamp)
+    
+    def turn_on(self):
+        for lamp in self.conjunto:
+            lamp.set_off()
+    
+    def get_conjunto_states(self):
+        result = []
+        for lamp in self.conjunto:
+            result.append(lamp.get_state())
